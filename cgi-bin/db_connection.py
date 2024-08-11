1bbd35af780ec170208e0f26a35ff64f26683724
Stephen "Christian" Kocsis <89398186+stephenkocsis14@users.noreply.github.com>
2024-08-11 14:24:25 -0400

    Update db_connection.py

--- cgi-bin/db_connection.py
+++ cgi-bin/db_connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="your_username",
-        password="your_password",
+        user="skocsis2",
+        password="spCk7725!",
         database="multiomics"
     )
