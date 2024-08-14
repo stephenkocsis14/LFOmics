0c5e9a29006de0fd14aad58ec4d8dd060e970c4c
Stephen "Christian" Kocsis <89398186+stephenkocsis14@users.noreply.github.com>
2024-08-13 20:48:55 -0400

    Delete cgi-bin/utils directory

--- cgi-bin/utils/data_processing.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python3
-
-import pandas as pd
-
-def process_input_data(file_path):
-    data = pd.read_csv(file_path)
-    processed_data = data.apply(lambda x: (x - x.mean()) / x.std())
-    return processed_data
